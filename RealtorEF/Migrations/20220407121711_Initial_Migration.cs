007c9b7cfe8ae3d5b1f3084c3d779721e4f44cd2
EightLegs <esmitin17@mail.ru>
2022-04-07 15:18:34 +0300

    Initial miggration

---
+++ RealtorEF/Migrations/20220407121711_Initial_Migration.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealtorEF.Migrations
+{
+    public partial class Initial_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BuildingMaterials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BuildingMaterials", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Districts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Districts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EvaluationCriteria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EvaluationCriteria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Realtors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Realtors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RealEstate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DistrictId = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Floor = table.Column<int>(type: "int", nullable: false),
+                    RoomCount = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BuildingMaterialId = table.Column<int>(type: "int", nullable: false),
+                    Square = table.Column<double>(type: "float", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstate", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RealEstate_BuildingMaterials_BuildingMaterialId",
+                        column: x => x.BuildingMaterialId,
+                        principalTable: "BuildingMaterials",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RealEstate_Districts_DistrictId",
+                        column: x => x.DistrictId,
+                        principalTable: "Districts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Evaluations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RealEstateId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EvaluationCriteriaId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evaluations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Evaluations_EvaluationCriteria_EvaluationCriteriaId",
+                        column: x => x.EvaluationCriteriaId,
+                        principalTable: "EvaluationCriteria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Evaluations_RealEstate_RealEstateId",
+                        column: x => x.RealEstateId,
+                        principalTable: "RealEstate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RealEstateId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RealtorId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sales_RealEstate_RealEstateId",
+                        column: x => x.RealEstateId,
+                        principalTable: "RealEstate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sales_Realtors_RealtorId",
+                        column: x => x.RealtorId,
+                        principalTable: "Realtors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evaluations_EvaluationCriteriaId",
+                table: "Evaluations",
+                column: "EvaluationCriteriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evaluations_RealEstateId",
+                table: "Evaluations",
+                column: "RealEstateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstate_BuildingMaterialId",
+                table: "RealEstate",
+                column: "BuildingMaterialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstate_DistrictId",
+                table: "RealEstate",
+                column: "DistrictId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_RealEstateId",
+                table: "Sales",
+                column: "RealEstateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_RealtorId",
+                table: "Sales",
+                column: "RealtorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Evaluations");
+
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "EvaluationCriteria");
+
+            migrationBuilder.DropTable(
+                name: "RealEstate");
+
+            migrationBuilder.DropTable(
+                name: "Realtors");
+
+            migrationBuilder.DropTable(
+                name: "BuildingMaterials");
+
+            migrationBuilder.DropTable(
+                name: "Districts");
+        }
+    }
+}
